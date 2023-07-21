c7175b2b2bd88fd4c77949a9c625a0530a685207
TruongNguyen1305 <trngcr71305@gmail.com>
2023-07-21 14:00:43 +0700

    update swap2chain contract

--- contracts/SwapTwoChain.sol
+++ contracts/SwapTwoChain.sol
@@ -40,6 +40,7 @@ contract SwapTwoChain {
         address receiver;
         ERC20 tokenContract;
         uint256 amount;
+        string key;
         bytes32 hashlock;
         Status status;
     }
@@ -93,82 +94,106 @@ contract SwapTwoChain {
 
     /**
      * @dev Create a new lock contract.
-     * @param id - ID of transaction
+     * @param id - ID of transaction in database which was hashed
      * @param receiver - Address of receiver. 
      * @param token - Address of token contract.
      * @param amount - Amount of token.
      * @param hashlock - Hash of secret key.
+     * @param isSeller - true if is seller, false is buyer
      */
     function create(
         bytes32 id,
         address receiver,  
         address token, 
         uint256 amount,
-        bytes32 hashlock
+        bytes32 hashlock,
+        bool isSeller
     ) public uniqueOrder(id) {
         require(msg.sender != receiver, "Transaction invalid");
+        bytes32 contractId = isSeller ? createContractId(id, msg.sender, receiver) : createContractId(id, receiver, msg.sender);
         
-        transactions[id] = LockContract({
+        transactions[contractId] = LockContract({
             sender: msg.sender,
             receiver: receiver,
             tokenContract: ERC20(token),
             amount: amount,
+            key: "",
             hashlock: hashlock,
             status: Status.PENDING
         });
 
-        require(transactions[id].tokenContract.transferFrom(msg.sender, address(this), amount), "Transfer to contract failed");
-        emit created(id, msg.sender);
+        require(transactions[contractId].tokenContract.transferFrom(msg.sender, address(this), amount), "Transfer to contract failed");
+        emit created(contractId, msg.sender);
     }
 
 
     /** 
      * @dev Withdraw token from lock contract.
-     * @param txId - ID of transaction.
+     * @param contractId - ID of transaction.
      * @param key - Secret key to unlock contract.
      */
-    function withdraw(bytes32 txId, string memory key) external orderExisted(txId) orderInProgress(txId) {
-        LockContract storage exchangeTx = transactions[txId];
+    function withdraw(bytes32 contractId, string memory key) external orderExisted(contractId) orderInProgress(contractId) {
+        LockContract storage exchangeTx = transactions[contractId];
         
         require(exchangeTx.receiver == msg.sender, "Only receiver can withdraw");
         require(keccak256(abi.encodePacked(key)) == exchangeTx.hashlock, "Incorrect key");
 
         require(exchangeTx.tokenContract.transfer(exchangeTx.receiver, exchangeTx.amount), "Withdraw failed");
 
+        exchangeTx.key = key;
         exchangeTx.status = Status.WITHDRAWN;
 
-        emit withdrawn(txId, exchangeTx.receiver, exchangeTx.amount);
+        emit withdrawn(contractId, exchangeTx.receiver, exchangeTx.amount);
     }
 
 
     /**
      * 
-     * @param txId - ID of transaction.
+     * @param contractId - ID of transaction.
      * @param nonce - Nonce of account.
      * @param signatureAdmin - Signature of admin on txID, address, nonce.
      */
-    function refund(bytes32 txId, uint256 nonce, bytes memory signatureAdmin) orderExisted(txId) orderInProgress(txId) external {
+    function refund(bytes32 contractId, uint256 nonce, bytes memory signatureAdmin) orderExisted(contractId) orderInProgress(contractId) external {
         
-        bytes32 message = keccak256(abi.encodePacked( txId, msg.sender, nonce));
+        bytes32 message = keccak256(abi.encodePacked(contractId, msg.sender, nonce));
         bytes32 messageHash = ECDSA.toEthSignedMessageHash(message);
 
         require(admin == ECDSA.recover(messageHash, signatureAdmin), "Invalid signature");
         
-        LockContract storage exchangeTx = transactions[txId];
+        LockContract storage exchangeTx = transactions[contractId];
 
         require(exchangeTx.tokenContract.transfer(exchangeTx.sender,  exchangeTx.amount), "Refund failed");
 
         exchangeTx.status = Status.REFUNDED;
         
-        emit canceled(txId, exchangeTx.sender, exchangeTx.amount);
+        emit canceled(contractId, exchangeTx.sender, exchangeTx.amount);
     }
 
     /**
      * @dev Checks whether the lock contract is in progress or not.
-     * @param txId - ID of transaction.
+     * @param contractId - ID of transaction.
      */
-    function isInProgress(bytes32 txId) public view orderExisted(txId) returns(bool) {
-        LockContract memory exchangeTx = transactions[txId];
+    function isInProgress(bytes32 contractId) external view orderExisted(contractId) returns(bool) {
+        LockContract memory exchangeTx = transactions[contractId];
         return exchangeTx.status == Status.PENDING;
+    }
+
+    /** 
+     * @dev Get secret key of lock contract 
+     * @param contractId - ID of transaction.
+     */
+    function getSecretKey(bytes32 contractId) external view orderExisted(contractId) returns(string memory) {
+        require(msg.sender == transactions[contractId].sender, "Only owner can get secret key");
+        return transactions[contractId].key;
+    }
+
+    /**
+     * @dev Create contractId
+     * @param id - ID of transaction in database which was hashed.
+     * @param sender.
+     * @param receiver.
+     */
+    function createContractId(bytes32 id, address sender, address receiver) internal pure returns(bytes32) {
+        return keccak256(abi.encodePacked(id, sender, receiver));
     }
 }
